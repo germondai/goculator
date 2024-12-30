88372867c4d6d652a0cf67a10b9abdcc484cc5ac
germondai <bruno.krocek@gmail.com>
2024-12-30 01:22:52 +0100

    first calc

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var a, b int = 1, 2
+	var result = add(a, b)
+	fmt.Println("The result is", result)
+}
+
+func add(a, b int) int {
+	return a + b
 }
